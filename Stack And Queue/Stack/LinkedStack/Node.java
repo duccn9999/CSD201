adc387bf7d1e428b3a4ef092d01225ae1451ad6e
Duc <100286938+MentallyHandicapped69@users.noreply.github.com>
2022-07-14 20:31:47 +0700

    Add files via upload

---
+++ Stack And Queue/Stack/LinkedStack/Node.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedstack;
+
+/**
+ *
+ * @author bbbvv
+ */
+public class Node {
+    public Object info;
+    public Node next;
+
+    public Node(Object x, Node p) {
+        info = x;
+        next = p;
+    }
+    public Node(Object x){
+        this(x,null);
+    }
+    
+    
+}
