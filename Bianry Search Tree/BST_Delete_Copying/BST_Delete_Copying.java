00d8497fc579fd075b58e49ce634c3175134b896
Duc <100286938+MentallyHandicapped69@users.noreply.github.com>
2022-07-14 20:04:34 +0700

    Create BST_Delete_Copying.java

---
+++ Bianry Search Tree/BST_Delete_Copying/BST_Delete_Copying.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bst_delete_copying;
+
+/**
+ *
+ * @author bbbvv
+ */
+public class BST_Delete_Copying {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BSTree t = new BSTree();
+        System.out.println("\n After inserting 3, 5, 1, 5, 9, 8, 10: ");
+        int[] a = {3, 5, 1, 5, 9, 8, 10};
+        t.addArray(a);
+        System.out.println("\nPre-Order tranverse:");
+        t.preOrder(t.root);
+        System.out.println("\nIn-Order tranverse:");
+        t.inOrder(t.root);
+        System.out.println("\nPost-Order tranverse:");
+        t.postOrder(t.root);
+        System.out.println();
+    }
+
+}
