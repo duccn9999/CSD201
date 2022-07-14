eafae2df0754f5d04d76b05b328775051ded13c1
Duc <100286938+MentallyHandicapped69@users.noreply.github.com>
2022-07-14 20:23:51 +0700

    Add files via upload

---
+++ Sorting Algorithms/MergeSortWithLoop/MergeSortWithLoop.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mergesortwithloop;
+
+/**
+ *
+ * @author bbbvv
+ */
+public class MergeSortWithLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int[] b = {7, 3, 5, 9, 11, 8, 6, 15, 10, 12, 14};
+        EffSort t = new EffSort(b);
+        System.out.println("Before sort");
+        t.Display();
+        System.out.println("After sorted");
+        t.mergeSort();
+        t.Display();
+        System.out.println();
+    }
+    
+}
