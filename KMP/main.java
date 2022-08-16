b0b5b0e607a990b3c15473546f0e1197c4943d22
春春 <100286938+MentallyHandicapped69@users.noreply.github.com>
2022-08-16 21:58:36 +0700

    Create main.java

---
+++ KMP/main.java
@@ -0,0 +1,11 @@
+package KMP;
+
+public class main {
+    public static void main(String[] args) {
+        String_Matching t = new String_Matching();
+        String a = "0123456789";
+        String p = "567";
+        t.testSearch(a,p);
+        System.out.println();
+    }
+}
